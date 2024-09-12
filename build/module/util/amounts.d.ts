075675a8fc54740a8436a7f8526626cdd3006d72
kkkkk-npc <kkkkknpc@proton.me>
2024-09-12 22:08:25 +0800

    feat:update v3-sdk version

--- build/module/util/amounts.d.ts
+++ build/module/util/amounts.d.ts
@@ -3,5 +3,5 @@ import { FeeAmount } from '@uniswap/v3-sdk';
 export declare class CurrencyAmount extends CurrencyAmountRaw<Currency> {
 }
 export declare function parseAmount(value: string, currency: Currency): CurrencyAmount;
-export declare function parseFeeAmount(feeAmountStr: string): FeeAmount;
+export declare function parseFeeAmount(feeAmountStr: string): FeeAmount.LOWEST | FeeAmount.LOW | FeeAmount.MEDIUM | FeeAmount.HIGH;
 export declare function unparseFeeAmount(feeAmount: FeeAmount): "10000" | "3000" | "500" | "100";
